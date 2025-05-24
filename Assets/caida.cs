1a1e0bb733cad4b4da1c8e2d33188b967e600cab
tincho20087 <48802391@est.ort.edu.ar>
2025-05-24 00:52:35 -0300

    some more objects

---
+++ Assets/caida.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class caida : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool activado = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true; // Físicamente inactivo al inicio
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!activado)
+        {
+            rb.isKinematic = false; // Se activa cuando choca
+            activado = true;
+        }
+    }
+
+    void Update()
+    {
+        // si querés hacer algo frame a frame, va acá
+    }
+}
