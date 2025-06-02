91c8d7a1c8cff4c3a7541901a81133da1c2417b7
tincho20087 <48802391@est.ort.edu.ar>
2025-06-01 23:31:53 -0300

    caja and player script

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        Destroy(collision.gameObject);
+    }
+}
